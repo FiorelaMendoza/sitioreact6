76a31cdec7c575d292398014eb6dea1b9a4b0ee6
FiorelaMendoza <fio.mendoza.capira@gmail.com>
2023-11-10 09:54:31 -1200

    modifique nombre

--- src/App.js
+++ src/App.js
@@ -7,15 +7,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Fiorela Mendoza Capira
+        Turno mañana 6to Semestre
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          heret
         </a>
       </header>
     </div>
